af7fbcc11ccf0b76c33daab919ed11cc20832da8
Scott Holden <ssh@sshconnection.com>
2016-07-28 19:56:11 -0700

    flight search

---
+++ flight.rb
@@ -0,0 +1,51 @@
+<<-FLIGHTS
+
+SFO--------------------------------JFK
+             DFW-------------------JFK
+      SFO---------------DFW
+                             DFW-------------------JFK
+DFW--------------------------------JFK
+
+      SFO----LAS        LAS-----------------------------JFK
+12    4      6          8    9      12             4    5
+
+FLIGHTS
+
+require 'date'
+require 'pp'
+
+Flight = Struct.new(:depart, :land, :depart_at, :land_at) do
+  def self.build(depart, land, depart_at, land_at)
+    new(depart, land, DateTime.parse(depart_at), DateTime.parse(land_at))
+  end
+end
+
+FLIGHTS = [
+  Flight.build(:sfo, :jfk, '2016-07-28 12:00 AM', '2016-07-28 12:00 PM'),
+  Flight.build(:dfw, :jfk, '2016-07-28  6:00 AM', '2016-07-28 12:00 PM'),
+  Flight.build(:sfo, :dfw, '2016-07-28  4:00 AM', '2016-07-28  8:00 AM'),
+  Flight.build(:dfw, :jfk, '2016-07-28  9:00 AM', '2016-07-28  4:00 PM'),
+  Flight.build(:dfw, :jfk, '2016-07-28 12:00 AM', '2016-07-28 12:00 PM'),
+  Flight.build(:sfo, :las, '2016-07-28  4:00 AM', '2016-07-28  6:00 AM'),
+  Flight.build(:sfo, :las, '2016-07-28  8:00 AM', '2016-07-28  5:00 PM')
+]
+
+def get_arrival_paths(flights, depart, land, depart_at, path = [])
+  departures = flights.select{|f| f.depart == depart && f.depart_at >= depart_at}
+  arrivals, connections = departures.partition{|f| land == f.land}
+
+  arrival_paths = arrivals.map{|f| path + [f]}
+  arrival_paths += connections.flat_map do |f|
+    get_arrival_paths(flights, f.land, land, f.land_at, path + [f])
+  end
+
+  arrival_paths
+end
+
+def get_best_path(flights, depart, land, depart_at)
+  paths = get_arrival_paths(flights, depart, land, depart_at)
+  paths.sort_by{|path| path.last.land_at }.first
+end
+
+pp get_arrival_paths(FLIGHTS, :sfo, :jfk, DateTime.parse('2016-07-28 12:00 AM'))
+pp get_best_path(FLIGHTS, :sfo, :jfk, DateTime.parse('2016-07-28 12:00 AM'))
